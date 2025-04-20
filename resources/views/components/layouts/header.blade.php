dccdb42bcb2f80d5cd40506b99b2d4b9527ef6c9
megumi seki <sggmm712@gmail.com>
2025-04-20 19:37:03 +0900

    Initial commit

---
+++ resources/views/components/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<div class="header">
+    <img class="icon" src="{{ asset("img/icon.png") }}" alt="icon">
+    <div class="header-navs">
+        <button class="nav-btn">Signup</button>
+        <button class="nav-btn">Login</button>
+    </div>
+</div>
---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,7 @@
+<x-base-layout>
+    <x-layouts.header />
+    <main>
+        ao
+        {{ $slot }}
+    </main>
+</x-base-layout>
