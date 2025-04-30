78f7ae8e69957e0d10f1aee9e422e78cca00ea82
megumi seki <sggmm712@gmail.com>
2025-04-30 09:52:01 +0900

    update profile page

--- resources/views/components/heart-big.blade.php
+++ resources/views/components/heart-big.blade.php
@@ -2,7 +2,7 @@
 
 <button class="btn-def-unset">
     <img src="{{ asset('img/heart-with-colors.png') }}" alt="" 
-        class="heart-big ml-minus toggle {{ $isSentHeart ? '' : 'hidden' }}">
+        class="heart-big ml-minus-smaller toggle {{ $isSentHeart ? '' : 'hidden' }}">
     <img src="{{ asset('img/heart-transparent.png') }}" alt="" 
-        class="heart-t-big ml-minus toggle {{ !$isSentHeart ? '' : 'hidden' }}">
+        class="heart-t-big ml-minus-smaller toggle {{ !$isSentHeart ? '' : 'hidden' }}">
 </button>
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,25 +1,41 @@
 <x-app-layout mainPadding="pt-medium" gap="gap-2" pageTitle="Profile" taCenter="ta-center">
 <div>
     <p class="bold">Your Profile Information</p>
-    <form action="#" method="GET" class="flex-col align-center gap-small mtb-small">
+    <form action="#" method="GET" class="flex-col gap-small align-center mtb-small">
         @csrf
-        <div class="relative">
-        <div class="flex">
-            <button name="user-icon" id="" class="user-icon-input">
-                <img src="{{ $user->userIcon->image_path }}" alt="User Icon" class="user-icon-profile">
-            </button>
-            <input type="text" name="name" placeholder="Name" class="input-def name-input" value="{{ $user->name }}" >
+        <div class="form-group">
+            <label for="name" class="font-small pl-smaller">Name</label>
+            <input type="name" id="name" name="name" placeholder="Name" class="input-def" value="{{ $user->name }}">
+        </div>        
+        <div class="form-group">
+            <label for="email" class="font-small pl-smaller">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email" class="input-def" value="{{ $user->email }}">
+        </div>        
+        <div class="form-group">
+            <label for="phone" class="font-small pl-smaller">Phone</label>
+            <input type="text" id="phone" name="phone" placeholder="Phone" class="input-def" value="{{ $user->phone }}">
         </div>
-        <div class="icon-list hidden">
-            <img src="#" alt="" class="icon-li">
-            <img src="#" alt="" class="icon-li">
-            <img src="#" alt="" class="icon-li">
-            <img src="#" alt="" class="icon-li">
-            <img src="#" alt="" class="icon-li">
+        <div class="form-group">
+            <div>
+                <label for="user-icon" class="font-small ml-minus-small">User Icon</label>
+                <label for="user-name" class="font-small pl-smaller">User Name</label>
+            </div>
+            <div class="relative">
+                <div class="flex">
+                    <button name="user-icon" id="user-icon" class="user-icon-input" type="button">
+                        <img src="{{ $user->userIcon->image_path }}" alt="User Icon" class="user-icon-profile">
+                    </button>
+                    <input type="text" name="user-name" id="user-name" placeholder="User Name" class="input-def name-input" value="{{ $user->user_name }}" >
+                </div>
+                <div class="icon-list hidden">
+                    <img src="#" alt="" class="icon-li">
+                    <img src="#" alt="" class="icon-li">
+                    <img src="#" alt="" class="icon-li">
+                    <img src="#" alt="" class="icon-li">
+                    <img src="#" alt="" class="icon-li">
+                </div>
+            </div>
         </div>
-        </div>
-        <input type="email" name="email" placeholder="Email" class="input-def" value="{{ $user->email }}">
-        <input type="text" name="phone" placeholder="Phone" class="input-def" value="{{ $user->phone }}">
         <div class="flex gap-1 justify-end">
             <button class="btn">Reset</button>
             <button class="btn">Update</button>
@@ -31,9 +47,18 @@
     <p class="bold">Update Password</p>
     <form action="#" method="GET" class="flex-col align-center gap-small mtb-small">
         @csrf
-        <input type="passwprd" name="current_password" placeholder="Current password" class="input-def">
-        <input type="password" name="new_password" placeholder="New password" class="input-def">
-        <input type="password" name="new_password_confirmation" placeholder="Repeat password" class="input-def">
+        <div class="form-group">
+            <label for="current-password" class="font-small pl-smaller">Current Password</label>
+            <input type="password" id="current-password" name="current_password" placeholder="Current password" class="input-def">
+        </div>
+        <div class="form-group">
+            <label for="new-password" class="font-small pl-smaller">New Password</label>
+            <input type="password" name="new_password" placeholder="New password" class="input-def">
+        </div>
+        <div class="form-group">
+            <label for="new-password-confirmation" class="font-small pl-smaller">Repeat Password</label>
+            <input type="password" name="new_password_confirmation" placeholder="Repeat password" class="input-def">
+        </div>
         <div class="flex gap-1 justify-end">
             <button class="btn">Update password</button>
         </div>
