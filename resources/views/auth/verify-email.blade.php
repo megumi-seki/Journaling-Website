f0b79159d8909ec7b0b11e24ceddd9963e379659
megumi seki <sggmm712@gmail.com>
2025-05-07 12:20:47 +0900

    update email verification send view

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,3 +1,12 @@
-<x-app-layout>
-    email verification notice
+<x-app-layout mainPadding="pt-small" pageTitle="Everyone's Journals">
+    <form action="{{ route('verification.send') }}" method="POST"
+        class="auth-form-width  m-auto mtb-small">
+        @csrf
+        <div class="form-group mtb-small font-small">
+            <h2 class="title">Verify Your Email Address</h2>
+            <p class="mtb-smaller">Before proceeding, please check your email for a verification link.
+            If you did not receive the email,</p>
+            <button class="primary-btn mtb-smaller">Please click here to request another link</button>
+        </div>
+    </form>
 </x-app-layout>
